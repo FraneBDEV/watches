c4b5816fa8f2a6dd65a4c3cdf11f0552dfd80eb7
FraneBDEV <franekbialoskorski@wp.pl>
2022-03-22 22:41:32 +0100

    main

--- src/index.js
+++ src/index.js
@@ -2,20 +2,12 @@ import { BrowserRouter } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './routes';
-import StartPage from './components/StartPage';
-import News from './components/News';
-import ContactForm from "./components/ContactForm";
-import Service from "./components/Service";
 
 
 
 ReactDOM.render(
-    <>
-    <StartPage />
-    <News />
-    <ContactForm />
-    <Service />
-    </>
-    , 
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>, 
     document.getElementById('root')
 )
